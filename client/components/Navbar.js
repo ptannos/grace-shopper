d2f23501f7b1e3d42e16f6685830acfe0043b6a2
Pauline Tannos <ptannos2@gmail.com>
2021-06-17 12:14:52 -0400

    updated signup and working on cart

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -7,25 +7,28 @@ import { logout } from "../store";
 const Navbar = ({ handleClick, isLoggedIn }) => (
   <div>
     <h1>Plate Hopper</h1>
-    <Home />
-    <nav>
-      <Link to="/">Home</Link>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
+    <div className="nav-container">
+      <Home />
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/cart/:userId/:orderId">View Cart</Link>
+        {isLoggedIn ? (
+          <div>
+            {/* The navbar will show these links after you log in */}
+            <a href="#" onClick={handleClick}>
+              Logout
+            </a>
+          </div>
+        ) : (
+          <div>
+            {/* The navbar will show these links before you log in */}
+            <Link to="/login">Login</Link>
+
+            <Link to="/signup">Sign Up</Link>
+          </div>
+        )}
+      </nav>
+    </div>
   </div>
 );
 
