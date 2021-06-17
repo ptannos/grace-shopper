b5d55a78c4a7268272d29b4a803148f2555686bb
Pauline Tannos <ptannos2@gmail.com>
2021-06-17 16:31:09 -0400

    guest cart

--- client/store/cart.js
+++ client/store/cart.js
@@ -3,10 +3,22 @@ const TOKEN = "token";
 
 //Action types
 const SET_CART = "SET_CART";
+const GET_GUEST_CART = "GET_GUEST_CART";
+const SAVE_GUEST_CART = "SAVE_GUEST_CART";
 
 //Action creators
 const setCart = (cart) => ({
   type: SET_CART,
+  cart,
+});
+
+const getGuestCart = (cart) => ({
+  type: GET_GUEST_CART,
+  cart,
+});
+
+const saveGuestCart = (cart) => ({
+  type: SAVE_GUEST_CART,
   cart,
 });
 
@@ -27,10 +39,36 @@ export const fetchCart = () => {
   };
 };
 
+export const fetchGuestCart = () => {
+  return async (dispatch) => {
+    try {
+      const guestCart = JSON.parse(localStorage.getItem("cart"));
+      dispatch(getGuestCart(guestCart));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+export const _saveGuestCart = (cart) => {
+  return async (dispatch) => {
+    try {
+      const guestCart = JSON.stringify(localStorage.setItem("cart", cart));
+      dispatch(saveGuestCart(guestCart));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
 //Reducer
 export default function (state = {}, action) {
   switch (action.type) {
     case SET_CART:
+      return action.cart;
+    case GET_GUEST_CART:
+      return action.cart;
+    case SAVE_GUEST_CART:
       return action.cart;
     default:
       return state;
