486ec1c0a211a26b364af059706e0eadd012e642
Victoria Kim <vkim20@gmail.com>
2021-06-16 23:30:53 -0400

    cleaned up routes & navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,36 +1,32 @@
 import React from "react";
 import { connect } from "react-redux";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import SingleProduct from "./SingleProduct";
-import Cart from "./Cart";
+import { Link } from "react-router-dom";
+import Home from "./Home";
 import { logout } from "../store";
 
 const Navbar = ({ handleClick, isLoggedIn }) => (
-  <Router>
-    <div>
-      <h1>Plate Hopper</h1>
-      <nav>
-        {isLoggedIn ? (
-          <div>
-            {/* The navbar will show these links after you log in */}
-            <Link to="/home">Home</Link>
-            <a href="#" onClick={handleClick}>
-              Logout
-            </a>
-          </div>
-        ) : (
-          <div>
-            {/* The navbar will show these links before you log in */}
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
-          </div>
-        )}
-      </nav>
-      <hr />
-    </div>
-    <Route exact path="/products/:id" component={SingleProduct} />
-    <Route exact path="/cart/:orderId" component={Cart} />
-  </Router>
+  <div>
+    <h1>Plate Hopper</h1>
+    <Home />
+    <nav>
+      <Link to="/">Home</Link>
+      {isLoggedIn ? (
+        <div>
+          {/* The navbar will show these links after you log in */}
+          <a href="#" onClick={handleClick}>
+            Logout
+          </a>
+        </div>
+      ) : (
+        <div>
+          {/* The navbar will show these links before you log in */}
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Sign Up</Link>
+        </div>
+      )}
+    </nav>
+    <hr />
+  </div>
 );
 
 /**
